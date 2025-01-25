c52bf1d8959903c0ddb48e969cf3408f8b7b8ee2
Turbootzz <tc.herman@hotmail.com>
2025-01-25 13:04:24 +0100

    added PUT operation

---
+++ src/app/api/clips/[id]/route.ts
@@ -0,0 +1,38 @@
+import { connectToDatabase } from "@/lib/mongodb";
+import { Clip } from "@/models/Clip";
+import { NextResponse } from "next/server";
+
+interface Params {
+    params: {
+        id: string;
+    };
+}
+
+export async function PUT(req: Request, { params }: Params) {
+    try {
+        await connectToDatabase();
+        const url = new URL(req.url);
+        const id = params.id; // maybe different solution needed
+
+        if (!id) {
+            return NextResponse.json({ error: "Missing ID parameter" }, { status: 400 });
+        }
+
+        const { newTitle: title, newDescription: description } = await req.json();
+        if (!title || !description) {
+            return NextResponse.json({ error: "Invalid data provided" }, { status: 400 });
+        }
+
+        const clip = await Clip.findById(id);
+        if (!clip) {
+            return NextResponse.json({ error: "Clip not found" }, { status: 404 });
+        }
+
+        await Clip.findByIdAndUpdate(id, { title, description });
+        return NextResponse.json({ message: "Clip information updated." }, { status: 200 });
+
+    } catch (error: any) {
+        console.error("Error updating clip:", error);
+        return NextResponse.json({ error: "Failed to update clip" }, { status: 500 });
+    }
+}
