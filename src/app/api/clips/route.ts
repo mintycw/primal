bf4da35dda5b40680030d152cb969bda53100ea3
Turbootzz <tc.herman@hotmail.com>
2025-01-25 12:38:34 +0100

    added DELETE operation

--- src/app/api/clips/route.ts
+++ src/app/api/clips/route.ts
@@ -32,3 +32,25 @@ export async function POST(req: Request) {
 		return NextResponse.json({ error: "Failed to create clip" }, { status: 500 });
 	}
 }
+
+export async function DELETE(req: Request) {
+	try {
+		await connectToDatabase();
+		const url = new URL(req.url);
+		const id = url.searchParams.get("id");
+
+		if (!id) {
+			return NextResponse.json({ error: "Missing ID parameter" }, { status: 400 });
+		}
+
+		const clip = await Clip.findByIdAndDelete(id);
+		if (!clip) {
+			return NextResponse.json({ error: "Clip not found" }, { status: 404 });
+		}
+		return NextResponse.json({message: "Clip deleted"}, { status: 200 });
+
+	} catch (error: any) {
+		console.error("Error deleting clip:", error);
+		return NextResponse.json({ error: "Failed to delete clip" }, { status: 500 });
+	}
+}
